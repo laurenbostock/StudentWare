0ce5d19bac83c754071e36417cc175c56d76bb5a
Frankie <u1462480@unimil.hud.ac.uk>
2016-01-25 15:47:02 +0000

    Fully functional Google and Facebook JS files for social network login.

--- js/Google-Sign.js
+++ js/Google-Sign.js
@@ -5,23 +5,33 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
+    console.log('Name: ' + profile.getName(0));
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
+    var google_user_id = profile.getId();
+    var google_user_name = profile.getName(0);
+    var google_user_email = profile.getEmail();
+    var social_network = 'google';
+    social_network.toLowerCase();
+    document.cookie='verified_google_user_id'+'='+google_user_id;
+    document.cookie='verified_google_user_name'+'='+google_user_name;
+    document.cookie='verified_google_user_email'+'='+google_user_email;
+    document.cookie='social_network'+'='+social_network;
+    window.location.href='modulePage.php';
 
     /**
      * Need to connect to backend to verify so replace yourbackend.example/tokensignin
      * edit: I changed it to point to register.php
      * with appropriate PHP file
      */
-    var id_token = googleUser.getAuthResponse().id_token;
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:63342/studentware/register.php');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function () {
-        console.log('Signed in as: ' + xhr.responseText);
-    };
-    xhr.send('idtoken=' + id_token);
+    //var id_token = googleUser.getAuthResponse().id_token;
+    //var x = new XMLHttpRequest();
+    //x.open('POST', 'http://localhost:63342/studentware/register.php');
+    //x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    //x.onload = function () {
+    //    console.log('Signed in as: ' + x.responseText);
+    //};
+    //x.send('idtoken=' + id_token);
 }
 
 
@@ -51,7 +61,6 @@ function signOut() {
         console.log('User signed out.');
     });
 }
-
 
 function getUserData() {
     // auth2 is initialized with gapi.auth2.init() and a user is signed in.
